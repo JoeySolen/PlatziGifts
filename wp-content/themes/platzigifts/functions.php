4e81332d31c8c0fcb48daac6207d318bf16b6c5c
Jose <jgmorenor277@gmail.com>
2023-01-12 17:52:13 -0500

    Se vincularon los estilos CSS en el archivo functions php

--- wp-content/themes/platzigifts/functions.php
+++ wp-content/themes/platzigifts/functions.php
@@ -7,4 +7,14 @@
     }
 
     add_action( 'after_setup_theme','init_template' );
+
+    function assets(){
+        wp_register_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css', '', '4.4.1', 'all' );
+        wp_register_style('montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap', '', '1.0', 'all');
+
+        wp_enqueue_style( 'estilos',  get_stylesheet_uri(), array('bootstrap', 'montserrat'), '1.0', 'all');
+    }
+
+    add_action( 'wp_enqueue_scripts', 'assets' );
 ?>
+
